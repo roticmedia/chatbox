dde6faf03c6c15778579d7aec5befb61253cbf33
Milad Xandi <stun.m_zandizand@riau.ac.ir>
2021-01-25 14:26:21 +0330

    add feedback buttons

--- src/append.js
+++ src/append.js
@@ -23,6 +23,18 @@ function appendRemote(text) {
                 </div>
             </div>
         </article>
+        <div class="rotic-feedback">
+            <button type="button" class="btn btn-success btn-feedback-true">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check2" viewBox="0 0 16 16">
+                    <path d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0z"/>
+                </svg>
+            </button> 
+            <button type="button" class="btn btn-danger btn-feedback-false">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
+                    <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+                </svg>
+            </button>
+        </div>
     `;
 }
 
@@ -445,6 +457,19 @@ function appendChatbox() {
         word-break: break-word;
         font-size: 10pt;
         line-height: 1.5;
+      }
+      .rotic-feedback {
+        padding: 0;
+        margin-bottom: 10px;
+        font-size: 10px;
+      }
+      .btn-feedback-false {
+        margin-left: 16px;
+        font-size: 10px;
+      }
+      .btn-feedback-true {
+        font-size: 10px;
+        margin-left: 9px;
       }
     </style>`;
 }
