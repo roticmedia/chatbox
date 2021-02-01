9bac08453f5a58d382fefdde0ab6a6faa7805707
Milad Xandi <stun.m_zandizand@riau.ac.ir>
2021-02-01 16:47:48 +0330

    now rotic logo open new tab rather than open redierect to rotic.ir

--- src/append.js
+++ src/append.js
@@ -74,7 +74,7 @@ function appendToast(text, x, y) {
 function appendChatbox() {
     return `<div class="rotic-chatbox"> 
     <div class="rotic-close-box">
-        <a href="https://rotic.ir/fa-ir" id="rotic-image-logo-link"><img src="https://rotic.ir/images/logo/Theme.png" alt="rotic" class="rotic-image-logo__img"></a> 
+        <a href="https://rotic.ir/fa-ir" id="rotic-image-logo-link" target="_blank"><img src="https://rotic.ir/images/logo/Theme.png" alt="rotic" class="rotic-image-logo__img"></a> 
         <p class="rotic-image-logo__p">powered by </p>
         <div class="rotic-close-text">
             <svg xmlns="http://www.w3.org/2000/svg" height="24" fill="#5FC5C4" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z" fill="#000" /></svg>
